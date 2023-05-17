417e6c3ca9ad6152ea85d930f7cf757cf3f379f2
Derlis Arguello <derlisarguello01@gmail.com>
2023-05-17 08:11:21 -0400

    ValidAnagram added

---
+++ src/py/com/daas/leetcode/ValidAnagram.java
@@ -0,0 +1,39 @@
+package py.com.daas.leetcode;
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+
+    /*
+    Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+    An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+    Example 1:
+    Input: s = "anagram", t = "nagaram"
+    Output: true
+
+    Example 2:
+    Input: s = "rat", t = "car"
+    Output: false
+     */
+    public static void main(String[] args) {
+        ValidAnagram validAnagram = new ValidAnagram();
+        System.out.println(validAnagram.isAnagram("anagram", "nagaram"));
+        System.out.println(validAnagram.isAnagram("rat", "car"));
+    }
+
+    public boolean isAnagram(String s, String t) {
+        /*
+            s.length = t.length
+            a = sort(s)
+            b = sort(t)
+            a == b
+        */
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        return Arrays.equals(a, b);
+    }
+}
