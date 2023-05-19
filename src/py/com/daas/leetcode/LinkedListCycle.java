9b31d314bb599c2d4c493d69da0fd79502e7aecd
Derlis Arguello <derlisarguello01@gmail.com>
2023-05-18 23:59:42 -0400

    LinkedListCycle added

--- src/py/com/daas/leetcode/LinkedListCycle.java
+++ src/py/com/daas/leetcode/LinkedListCycle.java
@@ -1,47 +1,36 @@
 package py.com.daas.leetcode;
 
+import py.com.daas.leetcode.utils.LinkedListUtil.ListNode;
+
 public class LinkedListCycle {
 
-  public static class ListNode {
-    int val;
-    ListNode next;
+    public static void main(String[] args) {
+        LinkedListCycle list = new LinkedListCycle();
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode4 = new ListNode(-4, listNode2);
+        ListNode listNode0 = new ListNode(0, listNode4);
+        listNode2.next = listNode0;
+        ListNode head = new ListNode(3, listNode2);
 
-    ListNode() {
+        //LinkedListUtil.print(head);
+
+        System.out.println("isValid = " + list.hasCycle(head));
     }
 
-    ListNode(int val) {
-      this.val = val;
+    public boolean hasCycle(ListNode head) {
+        //[3,2,0,-4,2]
+        // 3, 2, 0, -4
+        // 2, -4, -4, 2
+        ListNode slowPtr = head;
+        ListNode fastPtr = head;
+        while (fastPtr != null && fastPtr.next != null) {
+            slowPtr = slowPtr.next;
+            fastPtr = fastPtr.next.next;
+            if (slowPtr == fastPtr)
+                return true;
+        }
+
+        return false;
     }
-
-    ListNode(int val, ListNode next) {
-      this.val = val;
-      this.next = next;
-    }
-  }
-
-  public static void main(String[] args) {
-    LinkedListCycle list = new LinkedListCycle();
-    ListNode listNode4 = new ListNode(-4, new ListNode(2));
-    ListNode listNode3 = new ListNode(0, listNode4);
-    ListNode listNode2 = new ListNode(2, listNode3);
-    ListNode listNode1 = new ListNode(3, listNode2);
-
-    System.out.println("isValid = " + list.hasCycle(listNode1));
-  }
-
-  public boolean hasCycle(ListNode head) {
-    if (head == null) return false;
-    // two pointers
-    ListNode slow = head;
-    ListNode fast = head.next;
-    while (slow != null && fast != null) {
-      if (slow == fast) return true;
-      slow = slow.next;
-      if (fast.next == null) break;
-      fast = fast.next.next;
-    }
-
-    return false;
-  }
 
 }
